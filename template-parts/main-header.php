fe1ec290eb0d836dba5650b1c22ca47e3d48eb38
lhas <luizhrqas@gmail.com>
2020-09-06 17:44:13 -0300

    Fix main-header and top-bar components for desktop and mobile devices

--- template-parts/main-header.php
+++ template-parts/main-header.php
@@ -2,27 +2,27 @@
     <div class="container">
 
     <div class="row align-items-center">
-        <div class="col-8 col-md-4">
+        <div class="col-12 col-md-4">
             <div class="site-branding">
                 <?php the_custom_logo(); ?>
             </div><!-- .site-branding -->
         </div>
-        <div class="col-4 col-md-8">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i></button>
-            <nav id="site-navigation" class="main-navigation">
-                <?php
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'menu-1',
-                        'menu_id'        => 'primary-menu',
-                    )
-                );
-                ?>
+        <div class="col-12 col-md-8">
+            <div class="menuwrapper">
+                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i></button>
                 <a href="#" class="primaryaction"><i class="fa fa-user"></i></a>
                 <a href="#" class="primaryaction"><span class="counter">0</span><i class="fa fa-shopping-cart"></i></a>
-            </nav><!-- #site-navigation -->
-        </div>
-        <div class="col-12">
+                <nav id="site-navigation" class="main-navigation">
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'menu-1',
+                            'menu_id'        => 'primary-menu',
+                        )
+                    );
+                    ?>
+                </nav><!-- #site-navigation -->
+            </div>
         </div>
     </div>
     </div> <!-- .container -->
