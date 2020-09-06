b6bf08c69be48b5b90d49f7f0652da71925d4c87
lhas <luizhrqas@gmail.com>
2020-09-06 17:17:26 -0300

    Add some fixes for main-header and top-bar components

--- template-parts/top-bar.php
+++ template-parts/top-bar.php
@@ -1,10 +1,10 @@
 <header class="top-bar">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-sm-4">
                 <a href="tel:+5551999930505" target="_blank"><i class="fa fa-phone"></i> (51) 9 9993-0505</a>
             </div>
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-sm-8">
                 <p>Stay connected</p>
                 <ul class="socialmedia">
                     <li>
