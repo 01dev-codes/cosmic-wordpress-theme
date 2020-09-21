cb8894ee6cdaa58ee1995e1be29849efbf002d01
lhas <luizhrqas@gmail.com>
2020-09-21 16:12:16 -0300

    finished blog stylesheet

--- archive.php
+++ archive.php
@@ -1,6 +1,11 @@
 <?php
 /**
- * The template for displaying archive pages
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -10,42 +15,43 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<aside class="page-breadcrumb">
+    <div class="container">
+   		<h2 class="title"><?php the_archive_title(); ?></h2>
+    	<?php woocommerce_breadcrumb(); ?>
+    </div>
+</aside>
 
-		<?php if ( have_posts() ) : ?>
+<main id="primary" class="site-main container blog py-4">
+    <div class="row">
+        <div class="col-12 col-md-8">
+        <?php if (have_posts() ) : ?>
+        <?php 
+        /* Start the Loop */
+        while (have_posts() ) :
+           the_post();
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+            /*
+                * Include the Post-Type-specific template for the content.
+                * If you want to override this in a child theme, then include a file
+                * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                */
+            get_template_part( 'template-parts/content', 'blog');
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+        endwhile;
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+        ?>
+        <div class="pagination">
+    <?php 
+        echo paginate_links();
+    ?>
+</div>
+        <?php endif; wp_reset_postdata(); ?>
+        </div>
+        <div class="col-12 col-md-4">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</main>
 
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,39 +1,52 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The main template file
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package _s
  */
 
 get_header();
 ?>
-	<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+<aside class="page-breadcrumb">
+    <div class="container">
+   		<h2 class="title"><?php the_title(); ?></h2>
+    	<?php woocommerce_breadcrumb(); ?>
+    </div>
+</aside>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+<main id="primary" class="site-main container blog py-4">
+    <div class="row">
+        <div class="col-12 col-md-8">
+        <?php if (have_posts() ) : ?>
+        <?php 
+        /* Start the Loop */
+        while (have_posts() ) :
+           the_post();
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', '_s' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', '_s' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+            /*
+                * Include the Post-Type-specific template for the content.
+                * If you want to override this in a child theme, then include a file
+                * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+                */
+            get_template_part( 'template-parts/content', 'single');
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+        endwhile;
 
-		endwhile; // End of the loop.
-		?>
+        ?>
+        <?php endif;  ?>
+        </div>
+        <div class="col-12 col-md-4">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</main>
 
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
