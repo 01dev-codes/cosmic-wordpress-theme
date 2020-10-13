39fa93a8f87f9bd7c1879a1c076dc63e46cb04af
lhas <luizhrqas@gmail.com>
2020-10-12 22:56:45 -0300

    update sidebar and top bar

--- template-parts/top-bar.php
+++ template-parts/top-bar.php
@@ -1,10 +1,7 @@
 <header class="top-bar">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-12 col-sm-4">
-                <a href="tel:+5551999930505" target="_blank"><i class="fa fa-phone"></i> (51) 9 9993-0505</a>
-            </div>
-            <div class="col-12 col-sm-8">
+            <div class="col-12">
                 <p>Stay connected</p>
                 <?php get_template_part( 'template-parts/social-media' ); ?>
             </div>
